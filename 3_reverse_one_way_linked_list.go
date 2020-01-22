fd3dc175c12359752220e9b18bcb8f5ea880f547
Nozim Yunusov <nozimdev@gmail.com>
2020-01-22 04:19:08 +0300

    feat()

---
+++ 3_reverse_one_way_linked_list.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	Data int
+	Next *Node
+}
+
+func main() {
+	head := &Node{Data: 1}
+	head.Next = &Node{Data: 2}
+	head.Next.Next = &Node{Data: 3}
+	head.Next.Next.Next = &Node{Data: 4}
+
+	head = reverse(head)
+
+	fmt.Printf("Reversed Linked List: \n")
+	printList(head)
+}
+
+func reverse(head *Node) *Node {
+	prev := head
+	current := head.Next
+	var next *Node
+
+	for current != nil {
+		next = current.Next
+		current.Next = prev
+		prev = current
+		current = next
+	}
+
+	head.Next = nil
+	head = prev
+
+	return head
+}
+
+func printList(head *Node) {
+	for head != nil {
+		fmt.Printf("%+v\n", head)
+
+		head = head.Next
+	}
+}
